6cef4595f0da8e748316de3859e561c92f0e0e33
Delmag1 <cheek_robert@hotmail.com>
2025-08-01 18:24:19 +0200

    1aug25

---
+++ functions/get_files_info.py
@@ -0,0 +1,45 @@
+import os
+
+
+def get_files_info(working_directory, directory="."):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.abspath(os.path.join(working_directory, directory))
+    if not target_dir.startswith(abs_working_dir):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
+    try:
+        files_info = []
+        for filename in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, filename)
+            file_size = 0
+            is_dir = os.path.isdir(filepath)
+            file_size = os.path.getsize(filepath)
+            files_info.append(
+                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            )
+        return "\n".join(files_info)
+    except Exception as e:
+        return f"Error listing files: {e}"
+
+import os
+from config import MAX_CHARS
+
+
+def get_file_content(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    try:
+        with open(abs_file_path, "r") as f:
+            content = f.read(MAX_CHARS)
+            if os.path.getsize(abs_file_path) > MAX_CHARS:
+                content += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
+        return content
+    except Exception as e:
+        return f'Error reading file "{file_path}": {e}'
